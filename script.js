16f8c6d20e6e018a530d17cb0429d0968d005438
Ruliam dos Santos de Oliveira <ruliam.oliveira@gmail.com>
2022-03-30 08:29:12 -0300

    Add files via upload
    Primeiro Commit

---
+++ script.js
@@ -0,0 +1,22 @@
+var myButtonModal = document.getElementById('myButtonModal')
+var myTextModal = document.getElementById('myTextModal')
+
+var inputUsuario = document.getElementById('inputUsuario')
+var inputSenha = document.getElementById('inputSenha')
+
+
+function autenticar(event){
+  // interromper o submit
+  event.preventDefault()
+
+  if(inputUsuario.value == 'root' && inputSenha.value == '1234')
+  {
+    myTextModal.className = 'text-success'
+    myTextModal.innerText = 'Usuário Logado!!'
+  } else{
+    myTextModal.className = 'text-danger'
+    myTextModal.innerText = 'Usuário ou senha inválida!'
+  }
+
+  myButtonModal.click()
+}
